cc1242bfc7d9f05f24ad3aa0fb3784415569dde9
ameybhivshet <ameybhivshet26995@gmail.com>
2017-03-18 13:48:04 +0530

    handling blank input

--- app/src/main/java/com/example/amey/ballsandoverscounter/MainActivity.java
+++ app/src/main/java/com/example/amey/ballsandoverscounter/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 EditText minovers;
@@ -15,15 +16,23 @@ EditText minovers;
         setContentView(R.layout.activity_main);
         minovers= (EditText) findViewById(R.id.minovers);
 
+
+
     }
         public void done(View view)
     {
         Intent intent;
         intent=new Intent(this,Counting.class);
-        intent.putExtra("ov",String.valueOf(minovers.getText()));
-        Log.d("a",String.valueOf(minovers.getText()));
-        startActivity(intent);
+        if(String.valueOf(minovers.getText())==" " ||Integer.parseInt(String.valueOf(minovers.getText()))==0 )
+        {
+            Toast.makeText(this, " enter a valid input ", Toast.LENGTH_SHORT).show();
+        }
+        else {
 
+            intent.putExtra("ov", String.valueOf(minovers.getText()));
+            Log.d("a", String.valueOf(minovers.getText()));
+            startActivity(intent);
+        }
 
     }
 
